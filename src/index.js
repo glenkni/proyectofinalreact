729c738a30e438abe9f5b6234afd8ee40e583856
glenkni <glenda.knight@hotmail.com>
2020-12-24 12:16:22 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from "./App";
-import NavBar from "./components/NavBar"
 
-ReactDOM.render(<NavBar/>, document.getElementById("root"))
+
+ReactDOM.render(<App/>, document.getElementById("root"))
