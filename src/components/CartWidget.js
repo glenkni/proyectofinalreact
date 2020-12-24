181a8e6b39b7498e65b830f81aa12d87c9b393f9
glenkni <glenda.knight@hotmail.com>
2020-12-24 12:15:56 +0100

    Create CartWidget.js

---
+++ src/components/CartWidget.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CartWidget() {
+    return (
+        <>
+        <img src="/img/fonimuebles.jpeg" alt="lgo foni muebles" style={{height:75}} > 
+        </img>
+        </>
+    )
+}
+
+export default CartWidget
