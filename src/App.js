f04c33d8b30e0e6dd1e5fafc8c89715938c63260
glenkni <glenda.knight@hotmail.com>
2020-12-31 10:56:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
-import ItemListContainer from './components/ItemListContainer';
+import ItemListContainer from './container/ItemListContainer';
+import ItemCount from './container/ItemCount'
 
 
 function App() {
+
+  
   return (
     <>
       <NavBar/>
       <ItemListContainer greeting="Bienvenido a mi sitio web" greeting2="Estamos trabajando para brindarte una mejor atención"/>
+      <ItemCount min={1} max={5} onAdd={()=> {console.log('lo agregaste!')}}/>
     </>
   );
 }
